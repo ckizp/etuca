563573512d5379daba7536ef9cedb5eec525cef6
JoshuaMrtl <joshua.martinoli@etu.univ-cotedazur.fr>
2024-02-16 15:56:17 -0500

    commentaire correction bug

--- web/js/comment.js
+++ web/js/comment.js
@@ -1,15 +1,23 @@
 $(document).ready(function() {
-    $(document).on('click', '#show-comment', function() {
-        var pubid = $(this).attr('pubid'); 
-
-        console.log(pubid);
+    console.log("Script chargé");
+    $(document).on('click', '.show-comment', function(e) {
+        var pubid = $(this).data('pubid');
+        console.log("Clic sur .show-comment détecté pour l'ID de publication :", pubid);
 
         $.ajax({
             url: 'index.php?action=comments&publication=' + pubid,
             type: 'post',
-            data: {'publication': pubid},
+            data: { 'publication': pubid },
+            beforeSend: function() {
+                console.log("Envoi de la requête AJAX pour l'ID de publication :", pubid);
+            },
             success: function(response) {
-                document.getElementById("comments").innerHTML = response;
+                console.log("Réponse reçue :", response);
+                $("#comments-" + pubid).html(response);
+                $("#comment-section-" + pubid).show();
+            },
+            error: function(xhr, status, error) {
+                console.log("Erreur AJAX :", error);
             }
         });
     });
